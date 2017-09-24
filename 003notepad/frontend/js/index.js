959fef696ed27c4016fa4e6ef20e7083491a10dd
meatball <meatbal.com>
2017-09-24 23:46:44 +0800

    完成了记事本前后端交互逻辑

--- 003notepad/frontend/js/index.js
+++ 003notepad/frontend/js/index.js
@@ -1,27 +1,173 @@
 /**
- * Created by Administrator on 2017-09-14.
+ * Created by 丸子 on 2017-09-14.
  */
 $(function () {
 
-  $(".add").click(function () {
-    var noAdd = $(".no_add");
-    var noText = ('<textarea class="no_text col-xs-11">请输入文本信息!</textarea>')
-    noAdd.append(noText);
-    $(".remove").click(function () {
-      noText.remove();
-    })
+  // $(".add").click(function () {
+  //   var noAdd = $(".no_add");
+  //   var noText = ('<textarea class="no_text col-xs-11">请输入文本信息!</textarea>')
+  //   noAdd.append(noText);
+  //   $(".remove").click(function () {
+  //     noText.remove();
+  //   })
+  //
+  // })
 
-  })
-
-  $(".save").click(function () {
-
+  $("#save").click(function () {
+    var title = $("#note_title").val();
+    var content = $("#note_con ").val();
+    if(title==""||content==""){
+      alert("请输入内容！");
+      return;
+    }
     $.post('http://localhost:3000/note', {
-      title: '开学了',
-      content: '今天开学要好好学习'
-    }, function (status, data, message) {
-      console.log(status, data, message)
+      title: title,
+      content: content
+    }, function (data, status) {
+      console.log(data, status)
+      if (data.status == 0) {
+        $('#formAdd')[0].reset();
+        fetchList();
+      } else {
+        alert('创建失败')
+      }
     })
-
   })
 
+  var THS = ['序号', '日期', '标题', '内容', 'id', '操作'];
+
+  function getDeleteDom(item, index) {
+    return '<button class="btn  btn-danger btn-xs"  onclick="deleteItem(\'' + item._id + '\')">删除</button>';
+  }
+
+  function getEditDom(item, index) {
+    return '<button class="btn btn-success btn-xs" onclick="showEditDialog(\'' + item._id + '\')">编辑</button>';
+  }
+
+  var KEYS = [
+    function (item, index) {
+      return index + 1;
+    },
+    'createTime', 'title', 'content', '_id',
+    function (item, index) {
+      return [
+        getDeleteDom(item, index),
+        getEditDom(item, index)
+      ].join('&nbsp;');
+    }];
+
+  // 删除
+  window.deleteItem = function deleteItem(id) {
+    // console.log('deleteItem:', id);
+    $.ajax({
+      url: 'http://localhost:3000/note/' + id,
+      type: 'DELETE',
+      success: function (data, status) {
+        console.log(data, status);
+        fetchList();
+      }
+    });
+  }
+
+  // 编辑
+  window.showEditDialog = function showEditDialog(id) {
+    //TODO
+    var item = findItemById(id);
+    renderEditDialogContent(item);
+    $("#editDialog").modal();
+    console.log('showEditDialog:', id);
+  }
+
+  var EDIT_KEYS = ['title', 'content', '_id'];
+
+  function renderEditDialogContent(item) {
+    var editForm = $('#editDialog').find('form')[0];
+    EDIT_KEYS.forEach(function (key) {
+      var input = editForm[key];
+      input.value = item[key]
+    })
+  }
+
+// 搜索清除
+  $("#searchClear").click(function () {
+    $("#searchKey").val('');
+    fetchList();
+  })
+  function getFormData($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function (n, i) {
+      indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+  }
+
+  $('#editSave').click(function () {
+    var editData = getFormData($('#editDialog').find('form'))
+    console.log('editData:', editData);
+    // 保存修改
+    $.ajax({
+      url: 'http://localhost:3000/note/' + editData._id,
+      type: 'PUT',
+      data: editData,
+      success: function (data, status) {
+        $("#editDialog").modal('hide');
+        console.log(data, status);
+        fetchList();
+      }
+    });
+
+  })
+  $("#searchBtn").click(function () {
+    fetchList();
+  })
+  function findItemById(id) {
+    for (var i = 0; i < NOTES.length; i++) {
+      var item = NOTES[i];
+      if (id == item._id) {
+        return item;
+      }
+    }
+    return null;
+  }
+
+  function renderTable(list) {
+    var $table = $('<table class="table table-striped">');
+
+    var thTds = THS.map(function (item) {
+      return '<th>' + item + '</th>';
+    }).join('');
+
+    $('<thead>').append($('<tr>').append(thTds)).appendTo($table);
+
+    var $tbody = $('<tbody>').appendTo($table);
+    (list || []).forEach(function (item, index) {
+      var $tr = $('<tr>');
+      var trTds = KEYS.map(function (key) {
+        var tdContent;
+        if ($.isFunction(key)) {
+          tdContent = key(item, index);
+        } else {
+          tdContent = item[key];
+        }
+        return '<td>' + tdContent + '</td>';
+      }).join('')
+      $tbody.append($tr.append(trTds));
+    });
+    $('#table').empty().append($table);
+  }
+
+  var NOTES = null;
+
+  function fetchList() {
+    $.get('http://localhost:3000/note', {key: $("#searchKey").val()}, function (data, status) {
+      console.log(data, status);
+      var list = NOTES = data.data;
+      renderTable((list || []).reverse());
+    });
+  }
+
+  fetchList();
 })
--- 003notepad/routes/note.js
+++ 003notepad/routes/note.js
@@ -10,67 +10,80 @@ router.post('/', function (req, res) {
   var title = req.param('title');
   var content = req.param('content');
 
-  var note=new Note({title:title,content:content,createTime:Date.now()});
+  var note = new Note({title: title, content: content, createTime: Date.now()});
   note.save();
   console.log(title, content)
   res.json({
-    status:0,
-    msg:'save succes'
+    status: 0,
+    msg: 'save success'
   });
 });
 // 获取所有笔记
 router.get('/', function (req, res) {
-  Note.find(function (req,notes) {
+  var key = req.param('key');
+  var query = {};
+  if (key) {
+    query = {
+      $or: [{
+        title: new RegExp(key, "i")
+      }, {
+        content: new RegExp(key, "i")
+      }]
+    };
+  }
+  Note.find(query, function (req, notes) {
     console.log(notes);
     res.json({
-      status:0,
-      msg:"获取成功",
-      data:notes
+      status: 0,
+      msg: "获取成功",
+      data: notes
     })
   })
 });
-// 删除所有笔记
+// 删除指定id笔记
 router.delete('/:id', function (req, res, next) {
   var id = req.param('id');
-  Note.find({_id:id}).remove().exec();
+  Note.find({_id: id}).remove().exec();
   res.json({
-    status:0,
-    msg:"删除成功"
+    status: 0,
+    msg: "删除成功"
   })
 });
 module.exports = router;
 
 // 修改笔记
-router.put('/:id',function(req,res,next){
-  var id=req.param('id');
-  Note.findOne({_id:id} ,function (err,note) {
-    if(note != null){
+router.put('/:id', function (req, res, next) {
+  var id = req.param('id');
+  Note.findOne({_id: id}, function (err, note) {
+    if (note != null) {
       var title = req.param('title');
       var content = req.param('content');
-      note.title=title;
-      note.content=content;
+      note.title = title;
+      note.content = content;
       note.save();
       res.json({
-        status:0,
-        msg:"修改成功"
+        status: 0,
+        msg: "修改成功"
       })
-    }else{
+    } else {
       res.json({
-        status:-1,
-        msg:"数据不存在"
-        })
+        status: -1,
+        msg: "数据不存在"
+      })
     }
   })
 });
+
 router.get('/uncle', function (req, res) {
   var timeBefore = req.param('timeBefore');
   Note.find({
-    createTime:{$gt:timeBefore} },(function (req,notes) {
+    createTime: {$gt: timeBefore}
+  }, (function (req, notes) {
     console.log(notes);
     res.json({
-      status:0,
-      msg:"获取成功",
-      data:notes
+      status: 0,
+      msg: "获取成功",
+      data: notes
     })
   }))
 });
